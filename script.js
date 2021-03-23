c56c5d1a89eea0196201cb725b2288c71c52d05a
Caiqueoac <caiqueoc@gmail.com>
2021-03-23 00:14:25 -0300

    Desenhar comida

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ snake[0] = {
 }
 let direction= "right";
 
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box, 
+    y: Math.floor(Math.random() * 15 + 1) * box 
+
+}
+
 function criarBG( ) {
     context.fillStyle="lightgreen";
     context.fillRect(0 , 0 , 16*box, 16*box);    
@@ -21,6 +27,12 @@ function criarCobrinha( ) {
     }
     
 }
+
+function drawFood(params) {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 
 
 document.addEventListener('keydown', update);
@@ -45,6 +57,7 @@ function iniciarJogo(params) {
     
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
